aad4a729be3206f647b1053d0cdf852e6e46e328
Sebas Sebas <Sebas Sebas>
2016-09-12 15:34:38 +0200

    added files

---
+++ myScript.js
@@ -0,0 +1,15 @@
+function cambiaTexto () {
+			document.getElementById("change").innerHTML = "You did it!";
+		}
+function volverTexto () {
+			document.getElementById("change").innerHTML = "Press the button to change this text.";
+		}
+function bombillaOn () {
+			document.getElementById("bombilla").src='http://www.w3schools.com/js/pic_bulbon.gif';
+}
+function bombillaOff () {
+			document.getElementById("bombilla").src='http://www.w3schools.com/js/pic_bulboff.gif';
+}
+function contentDw () {
+			document.getElementById("dw").click();
+}
